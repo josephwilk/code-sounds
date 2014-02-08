07258d53982e89125fed47dfc2cdeceb784de68e
Joseph Wilk <joe@josephwilk.net>
2014-02-08 17:40:18 +0100

    removing lazyness for now

--- src/code_sounds/core.clj
+++ src/code_sounds/core.clj
@@ -3,41 +3,41 @@
             [clojure.java.io :as io])
   (:import [clojure.lang LineNumberingPushbackReader]))
 
-(def ^:private default-args {:init-ns    'user})
+(def init-ns 'user)
 
 (defn- careful-refer [ns]
   (binding [*ns* ns]
     (refer 'clojure.core :exclude (or (keys (ns-interns ns)) ())))
-  ns)
+    ns)
 
 (def eof (Object.))
 
-(defn read-file
-  "Generate a lazy sequence of top level forms from a
-   LineNumberingPushbackReader"
-  [^LineNumberingPushbackReader r init-ns]
-  (let [do-read (fn do-read [ns]
-                  (lazy-seq
-                   (let [form (binding [*ns* ns]
-                                (read r false eof))
-                         [ns? new-ns k] (when (sequential? form) form)
-                         ns (if (and (symbol? new-ns)
-                                     (or (= ns? 'ns) (= ns? 'in-ns)))
-                              (careful-refer (create-ns new-ns))
-                              ns)]
-                     (when-not (= form eof)
-                       (cons form (do-read ns))))))]
-    (do-read (careful-refer (create-ns init-ns)))))
+(defn expr-seq
+  "Given an expression (any piece of Clojure data), return a lazy (depth-first)
+  sequence of the expr and all its sub-expressions"
+  [expr]
+  (tree-seq sequential?
+            seq
+            expr))
 
-(defn check-reader [reader]
-  (read-file (LineNumberingPushbackReader. reader) (:init-ns default-args)))
-
-(defn check-file [source-file]
+(defn file->seq [source-file]
   (with-open [reader (io/reader source-file)]
-    (check-reader reader)))
+    (let [^LineNumberingPushbackReader r (LineNumberingPushbackReader. reader)
+          do-read (fn do-read [ns]
+                    (let [form (binding [*ns* ns] (read r false eof))
+                          [ns? new-ns k] (when (sequential? form) form)
+                          ns (if (and (symbol? new-ns)
+                                      (or (= ns? 'ns) (= ns? 'in-ns)))
+                               (careful-refer (create-ns new-ns))
+                               ns)]
+                      (when-not (= form eof) (cons form (do-read ns)))))]
 
-(defn run [source-file & args] (check-file (io/file source-file)))
+      (do-read (careful-refer (create-ns init-ns))))))
 
-(comment
-  (run  "/Users/josephwilk/Workspace/josephwilk/clojure/code-sounds/test/fixtures/bad_code.clj")
-  )
+(defn run [source-file & args]
+  (-> source-file
+      io/file
+      file->seq
+      expr-seq))
+
+(println (run "/Users/josephwilk/Workspace/josephwilk/clojure/code-sounds/test/fixtures/bad_code.clj"))
--- test/fixtures/bad_code.clj
+++ test/fixtures/bad_code.clj
@@ -1,3 +1,7 @@
-(ns fixtures.bad-code)
+(ns fixtures.bad-code
+  "An example bit of clojure code thats bad")
 
-(defn bad-fn [a b c d e f g h i j])
+(defn bad-fn [a b c d e f g h i j]
+  [a b c d e f g h i j])
+
+(defn pants-are-on-fire [])
